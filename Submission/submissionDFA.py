cceed98ad0ad345fadcae4cad343336427372351
Harsh <141917027+harshb20@users.noreply.github.com>
2026-03-28 15:26:01 +0530

    Fixed inclusion of optimizationusingDFA
    Added inclusion for optimising using DFA when fold or simp or both are enabled

--- Submission/submissionDFA.py
+++ Submission/submissionDFA.py
@@ -254,4 +254,9 @@ def optimize(irHandler, args):
         if not changed:
             break
             
+    # NEW: run old DFA-style optimization too, on updated IR
+    if args.opt_constprop or args.opt_dce or args.opt_all:
+        tempHandler = copy.deepcopy(irHandler)
+        tempHandler.ir = ir
+        ir = optimizeUsingDFA(tempHandler)        
     return ir
